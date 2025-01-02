69ed8550ed708cf859010d682ded7ca86d6a79f1
bifidokk <bifidokk@gmail.com>
2025-01-02 19:36:46 +0200

    Subscription app. Initial commit

---
+++ invoice/cmd/web/main.go
@@ -0,0 +1,106 @@
+package main
+
+import (
+	"database/sql"
+	"github.com/alexedwards/scs/redisstore"
+	"github.com/alexedwards/scs/v2"
+	"github.com/gomodule/redigo/redis"
+	"log"
+	"net/http"
+	"os"
+	"sync"
+	"time"
+
+	_ "github.com/jackc/pgconn"
+	_ "github.com/jackc/pgx/v4"
+	_ "github.com/jackc/pgx/v4/stdlib"
+)
+
+const (
+	webPort = 80
+)
+
+func main() {
+	db := initDb()
+	err := db.Ping()
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	defer db.Close()
+
+	session := initSession()
+	wg := &sync.WaitGroup{}
+	infoLog := log.New(os.Stdout, "INFO\t", log.Ldate|log.Ltime)
+	errorLog := log.New(os.Stderr, "ERROR\t", log.Ldate|log.Ltime|log.Lshortfile)
+
+	app := Config{
+		Session:  session,
+		DB:       db,
+		InfoLog:  infoLog,
+		ErrorLog: errorLog,
+		Wait:     wg,
+	}
+
+}
+
+func initDb() *sql.DB {
+	conn := connectToDB()
+
+	if conn == nil {
+		panic("failed to connect to database")
+	}
+
+	return conn
+}
+
+func connectToDB() *sql.DB {
+	dsn := os.Getenv("DSN")
+
+	connection, err := openDB(dsn)
+
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	return connection
+}
+
+func openDB(dsn string) (*sql.DB, error) {
+	db, err := sql.Open("pgx", dsn)
+
+	if err != nil {
+		return nil, err
+	}
+
+	err = db.Ping()
+
+	if err != nil {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+func initSession() *scs.SessionManager {
+	session := scs.New()
+	session.Store = redisstore.New(initRedis())
+	session.Lifetime = 24 * time.Hour
+	session.Cookie.Persist = true
+	session.Cookie.SameSite = http.SameSiteLaxMode
+	session.Cookie.Secure = true
+
+	return session
+}
+
+func initRedis() *redis.Pool {
+	pool := &redis.Pool{
+		MaxIdle: 10,
+		Dial: func() (redis.Conn, error) {
+			return redis.Dial("tcp", os.Getenv("REDIS"))
+		},
+	}
+
+	return pool
+}
