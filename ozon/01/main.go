b5342208bcc518ffcbe6d481f5ea342747c508fb
bifidokk <bifidokk@gmail.com>
2025-01-16 22:33:42 +0200

    algo 1

---
+++ ozon/01/main.go
@@ -0,0 +1,44 @@
+package main
+
+import (
+	"bufio"
+	"fmt"
+	"os"
+	"strconv"
+	"strings"
+)
+
+func main() {
+	reader := bufio.NewReader(os.Stdin)
+	writer := bufio.NewWriter(os.Stdout)
+	defer writer.Flush()
+
+	line, _ := reader.ReadString('\n')
+	t, _ := strconv.Atoi(strings.TrimSpace(line))
+
+	for i := 0; i < t; i++ {
+		line, _ = reader.ReadString('\n')
+		salaryString := strings.TrimSpace(line)
+
+		if len(salaryString) <= 1 {
+			fmt.Fprintln(writer, 0)
+			continue
+		}
+
+		// If remove the first "out of order" digit (the one that is smaller than the next), the number will remain the maximum.
+		indexToRemove := -1
+		for j := 0; j < len(salaryString)-1; j++ {
+			if salaryString[j] < salaryString[j+1] {
+				indexToRemove = j
+				break
+			}
+		}
+
+		if indexToRemove == -1 {
+			indexToRemove = len(salaryString) - 1
+		}
+
+		result := salaryString[:indexToRemove] + salaryString[indexToRemove+1:]
+		fmt.Fprintln(writer, result)
+	}
+}
