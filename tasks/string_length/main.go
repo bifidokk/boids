a1b1a4c2e2231d0036b7d3388d33fdfb2aefe8ac
bifidokk <bifidokk@gmail.com>
2025-02-09 16:27:46 +0200

    daily task

---
+++ tasks/string_length/main.go
@@ -0,0 +1,13 @@
+package main
+
+import (
+	"fmt"
+	"unicode/utf8"
+)
+
+func main() {
+	s := "abЯЮ漢"
+
+	fmt.Println(len(s))                    // 9 a, b - 2 bytes, ЯЮ - 4 bytes, 漢 - 3 bytes
+	fmt.Println(utf8.RuneCountInString(s)) // 5
+}
