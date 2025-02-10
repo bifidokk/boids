a5160f3f529ab45af82331b64c8a9cc0322770c2
bifidokk <bifidokk@gmail.com>
2025-02-10 20:56:26 +0200

    daily task

--- tasks/append_slice2/main.go
+++ tasks/append_slice2/main.go
@@ -4,13 +4,13 @@ import "fmt"
 
 func main() {
 	a1 := []int{1, 2, 3, 4, 5} // cap 5
-	fmt.Printf("ptr=%p len=%d cap=%d\n", &a1, len(a1), cap(a1))
+	fmt.Printf("ptr=%p len=%d cap=%d\n", &a1[0], len(a1), cap(a1))
 
 	a2 := append(a1, 6) // backed array changed
-	fmt.Printf("ptr=%p len=%d cap=%d\n", &a2, len(a2), cap(a2))
+	fmt.Printf("ptr=%p len=%d cap=%d\n", &a2[0], len(a2), cap(a2))
 
 	a3 := append(a1, 7) // backed array changed
-	fmt.Printf("ptr=%p len=%d cap=%d\n", &a3, len(a3), cap(a3))
+	fmt.Printf("ptr=%p len=%d cap=%d\n", &a3[0], len(a3), cap(a3))
 
 	fmt.Println(a1, a2, a3) // 12345, 123456, 123457
 }
