a40b30b24d0fd4e05ab07c3ccca7e3866aec06cc
bifidokk <bifidokk@gmail.com>
2025-02-10 20:55:30 +0200

    daily task

---
+++ tasks/append_slice2/main.go
@@ -0,0 +1,16 @@
+package main
+
+import "fmt"
+
+func main() {
+	a1 := []int{1, 2, 3, 4, 5} // cap 5
+	fmt.Printf("ptr=%p len=%d cap=%d\n", &a1, len(a1), cap(a1))
+
+	a2 := append(a1, 6) // backed array changed
+	fmt.Printf("ptr=%p len=%d cap=%d\n", &a2, len(a2), cap(a2))
+
+	a3 := append(a1, 7) // backed array changed
+	fmt.Printf("ptr=%p len=%d cap=%d\n", &a3, len(a3), cap(a3))
+
+	fmt.Println(a1, a2, a3) // 12345, 123456, 123457
+}
