479b0c0e4076ee4237b3b816902d8614712e4e52
bifidokk <bifidokk@gmail.com>
2025-02-16 16:15:35 +0200

    daily task

---
+++ tasks/interface/main.go
@@ -0,0 +1,26 @@
+package main
+
+type User struct {
+}
+
+func (u *User) Create() {}
+func (u *User) Get()    {}
+func (u *User) Delete() {}
+func (u *User) List()   {}
+
+type Reader interface {
+	Get()
+	List()
+}
+
+type Writer interface {
+	Create()
+	Delete()
+}
+
+func main() {
+	var userReader Reader = &User{}
+	userWriter := userReader.(Writer)
+	userWriter.Get() // can't call because made casting to Writer interface
+	_ = userWriter
+}
