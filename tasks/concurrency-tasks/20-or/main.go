453862c78bde2d9521ba7428252b9e0c553f49fb
bifidokk <bifidokk@gmail.com>
2025-03-29 17:16:39 +0200

    concurrency daily task

---
+++ tasks/concurrency-tasks/20-or/main.go
@@ -0,0 +1,61 @@
+package main
+
+import (
+	"fmt"
+	"time"
+)
+
+//Write a function "or" that returns a channel which closes either as soon as any of the channels in channels becomes available for reading,
+//or as soon as any of the channels in channels is closed.
+//In the main function, there is code which, when the or function works correctly, will output a number very close to one second.
+
+func or(channels ...<-chan interface{}) <-chan interface{} {
+	switch len(channels) {
+	case 0:
+		return nil
+	case 1:
+		return channels[0]
+	}
+
+	orDone := make(chan interface{})
+
+	go func() {
+		defer close(orDone)
+		switch len(channels) {
+		case 2:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			}
+		default:
+			select {
+			case <-channels[0]:
+			case <-channels[1]:
+			case <-or(append(channels[2:], orDone)...):
+			}
+		}
+	}()
+
+	return orDone
+}
+
+func main() {
+	sig := func(after time.Duration) <-chan interface{} {
+		c := make(chan interface{})
+		go func() {
+			defer close(c)
+			time.Sleep(after)
+		}()
+		return c
+	}
+
+	start := time.Now()
+	<-or(
+		sig(2*time.Hour),
+		sig(5*time.Minute),
+		sig(1*time.Second),
+		sig(1*time.Hour),
+		sig(1*time.Minute),
+	)
+	fmt.Printf("done after %v", time.Since(start))
+}
